7891af9d6df6a9ed35cc1b70185cf75a0325ef63
Alexis-rgb141 <alexrivgon0609@gmail.com>
2026-05-28 11:32:53 -0400

    feat: integracion completa de maqueta frontend con backend de productos

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const productoRoutes = require('./src/routes/productoRoutes');
+const productoRoutes = require('./routes/productoRoutes');
 
 const app = express();
 const PORT = 3000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/login-simulado', (req, res) => {
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.get('/api/login-simulado', (req, res) => {
     res.cookie('session_token', 'token_valido_123', { httpOnly: true });
     return res.send("Autenticado exitosamente. Cookie establecida.");
 });
+
 
 app.use('/api', productoRoutes);
 
